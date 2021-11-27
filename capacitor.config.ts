401f56738e683a67768c464ce0f437309dd960d5
mohamed sugroo <mohamedsugroo@gmail.com>
2021-11-27 18:15:09 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lotto',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
