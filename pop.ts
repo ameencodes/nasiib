2b1716de1808e09d37f8f3e337c014ee0a7f8fed
ameencodes <80363390+ameencodes@users.noreply.github.com>
2021-12-05 19:12:19 +0300

    Update pop.ts

--- pop.ts
+++ pop.ts
@@ -9,7 +9,7 @@ var serviceAccount = require("../lib/api/lotto.json")
 
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
-        databaseURL: "https://lotto-48a43-default-rtdb.europe-west1.firebasedatabase.app"
+        databaseURL: ""
       });
       const db= admin.firestore()    ;
       let query = db.collection('TransactionsVolume');
